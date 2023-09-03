ee656e5a5f2705c34a41212d3f489cd17adc54e0
Jana Ristovska <76064707+ristovska1606@users.noreply.github.com>
2023-09-03 14:34:50 +0200

    updated LetterWithWordsComponent and created WordComponent

--- wordsinfocus/src/components/LetterWithWordsComponent.vue
+++ wordsinfocus/src/components/LetterWithWordsComponent.vue
@@ -5,43 +5,107 @@
                 <h1 class="letter">{{ this.letter.name }}  </h1>
             </div>
             <div class="bottom">
-               
+               <div class="search">
+                <input type="text" class="searchInput"/>
+               </div>
+               <div>
+                <select name="words" id="words" class="wordsDropdown">
+                    <option v-for="word in words" v-bind:key="word.id" value="word.id">{{word.word}}</option>
+                </select>
+               </div>
             </div>
         </div>
         <div class="split right">
-            
+            <!-- <div>
+                <div class="words">
+                    <h3 v-for="p in paginatedData.slice(0,15)" v-bind:key="p.id">
+                        {{p.word}} 
+                    </h3>
+                </div>
+                <div class="words">
+                    <h3 v-for="p in paginatedData.slice(15,30)" v-bind:key="p.id">
+                        {{p.word}} 
+                    </h3>
+                </div>
+                <div class="words">
+                    <h3 v-for="p in paginatedData.slice(30,45)" v-bind:key="p.id">
+                        {{p.word}} 
+                    </h3>
+                </div>
+                    
+                
+                <button @click="prevPage" :disabled="pageNumber==0">
+                    Previous
+                </button>
+                <button @click="nextPage" :disabled="pageNumber >= pagecount -1">
+                    Next
+                </button>
+            </div> -->
+            <div>
+               
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+
+    
+
     export default{
-        name: "LetterWithWordsComponent",
-        data() {
-            return {
-                name: this.$route.params.l,
-                letter: null,
-                words:  []
-            }
+    name: "LetterWithWordsComponent",
+    props: {
+        listData: {
+            type: Array,
+            required: true
         },
-        methods: {
-            async getLetter(){
-                const lettersApi = await fetch('http://localhost:9090/api/letters/byName/'+ this.name)
-                const result = await lettersApi.json();
-                this.letter = result;
-                
-            },
-            async getWords(){
-                const lettersApi = await fetch('http://localhost:9090/api/letters/' + this.letter.id + '/words')
-                const result = await lettersApi.json();
-                this.words = result;
-            }     
-        },
-        beforeMount(){
-            this.getLetter();
-            this.getWords();
+        size: {
+            type: Number,
+            required: false,
+            default: 30
         }
+    },
+    data() {
+        return {
+            name: this.$route.params.l,
+            letter: null,
+            words: [],
+            pageNumber: 0
+        };
+    },
+    methods: {
+        async getLetter() {
+            const lettersApi = await fetch('http://localhost:9090/api/letters/byName/' + this.name);
+            const result = await lettersApi.json();
+            this.letter = result;
+        },
+        async getWords() {
+            const lettersApi = await fetch('http://localhost:9090/api/letters/' + this.letter.id + '/words');
+            const result = await lettersApi.json();
+            this.words = result;
+        },
+        nextPage() {
+            this.pageNumber++;
+        },
+        prevPage() {
+            this.pageNumber--;
+        }
+    },
+    computed: {
+        pageCount() {
+            let l = this.words.length, s = this.size;
+            return Math.ceil(l / s);
+        },
+        paginatedData() {
+            const start = this.pageNumber * this.size, end = start + this.size;
+            return this.words.slice(start, end);
+        }
+    },
+    beforeMount() {
+        this.getLetter();
+        this.getWords();
     }
+}
 </script>
 
 <style scoped>
@@ -52,11 +116,10 @@
 .letter{
     background-color: whitesmoke;
     display: inline-block;
-    width: 80px;
-    padding: 50px;
+    min-width: 80px;
+    padding: 40px;
     margin-top: 98px;
     font-size:xx-large;
-    font-family: cursive;
     font-size: 50px;
     color: #89621c;
     box-shadow: 5px 5px 10px 0px;;
@@ -80,14 +143,38 @@
 }
 
 .top{
-    height: 50%;
+    height: 35%;
     width: 100%;
     
 }
 
 .bottom{
-    height: 50%;
+    height: 65%;
     /* background-color: pink; */
+}
+
+.wordsDropdown{
+    font-size:xx-large;
+    font-size: 20px;
+    color: #89621c;
+    box-shadow: 5px 5px 10px 0px;;
+    border-bottom: 1px solid #bb8a36;
+    border-radius: 30px;
+    background-color: whitesmoke;
+    text-align: center;
+    width: 280px;
+}
+
+.searchInput{
+    font-size: 17px;
+    box-shadow: 5px 5px 10px 0px;
+    border-bottom: 1px solid #bb8a36;
+    border-radius: 30px;
+    background-color: whitesmoke;
+    text-align: center;
+    width: 272px;
+    height: 22px;
+    margin-bottom: 10px;
 }
 
 </style>
